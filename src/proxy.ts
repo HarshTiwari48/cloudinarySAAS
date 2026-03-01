d87e125936127b4d0fe1e24f962ef6adb88bf677
Harsh Tiwari <tiwari.harsh.p@gmail.com>
2026-03-01 18:54:37 +0530

    addeed signin signup pages with clerk

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/signin', '/signup', '/home']);
+const isPublicRoute = createRouteMatcher(['/', '/sign-in', '/sign-up', '/home']);
 
 const isPublicApiRoute = createRouteMatcher([
     "/api/videos"
@@ -20,11 +20,11 @@ export default clerkMiddleware((auth, req) => {
     if(!userId) {
         //if user not logged in and tried to access a protected route
         if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
-        return NextResponse.redirect(new URL('/signin', req.url));
+        return NextResponse.redirect(new URL('/sign-in', req.url));
     }
     // if the request id for a protected API nd the user is not logged in
     if(isApiRequest && !isPublicApiRoute(req)) {
-        return NextResponse.redirect(new URL('/signin', req.url));
+        return NextResponse.redirect(new URL('/sign-in', req.url));
     }
     }
     return NextResponse.next();
